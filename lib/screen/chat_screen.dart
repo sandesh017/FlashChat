8e644126b0680f4ad594fef031a704ba5d415d8b
Sandesh <sandeshpoudel55.sp@gmail.com>
2021-07-23 18:22:32 +0545

    completed

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,10 +1,13 @@
-// import 'package:flashchat/screen/welcome_screen.dart';
+import 'package:flashchat/screen/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flashchat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:firebase_core/firebase_core.dart';// old version
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _fireStore = FirebaseFirestore.instance;
+User? loggedInUser;
 
 class ChatScreen extends StatefulWidget {
   static String id = 'chat';
@@ -13,11 +16,12 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final messageTextCOntroller = TextEditingController();
   String? messageText;
-  final _fireStore = FirebaseFirestore.instance;
+
   final _auth = FirebaseAuth.instance;
   //FirebaseUser loggedInUser; firbaseUser is changed into User.
-  User? loggedInUser;
+
   @override
   void initState() {
     super.initState();
@@ -42,14 +46,14 @@ class _ChatScreenState extends State<ChatScreen> {
   //     print(msg.data);
   //   }
   // }
-
-  void messageStream() async {
-    await for (var snapshot in _fireStore.collection('message').snapshots()) {
-      for (var msg in snapshot.docs) {
-        print(msg.data());
-      }
-    }
-  }
+//For Concept of STREAM
+  // void messageStream() async {
+  //   await for (var snapshot in _fireStore.collection('message').snapshots()) {
+  //     for (var msg in snapshot.docs) {
+  //       print(msg.data());
+  //     }
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -61,10 +65,10 @@ class _ChatScreenState extends State<ChatScreen> {
               icon: Icon(Icons.logout),
               onPressed: () {
                 //Implement logout functionality
-                // _auth.signOut();
-                // Navigator.popUntil(
-                //     context, ModalRoute.withName(WelcomeScreen.id));
-                messageStream();
+                _auth.signOut();
+                Navigator.popUntil(
+                    context, ModalRoute.withName(WelcomeScreen.id));
+                // messageStream();
               }),
         ],
         title: Text('⚡️Chat'),
@@ -75,6 +79,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            MessageStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -82,6 +87,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextCOntroller,
                       onChanged: (value) {
                         //Do something with the user input.
                         messageText = value;
@@ -91,6 +97,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   TextButton(
                     onPressed: () {
+                      messageTextCOntroller.clear();
                       //Implement send functionality.
                       _fireStore.collection('message').add({
                         'text': messageText,
@@ -107,6 +114,86 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MessageStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      //snapshot.data is dynamic and we can use QuerySnapshot properties
+      stream: _fireStore.collection('message').snapshots(),
+      builder: (BuildContext context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+        final message = snapshot.data!.docs.reversed;
+        List<MessageBubble> messageBubble = [];
+        for (var msg in message) {
+          final msgText = msg['text'];
+          final msgSender = msg['email'];
+          final currentUser = loggedInUser!.email;
+
+          
+          final msgBubble = MessageBubble(msgText, msgSender, currentUser==msgSender);// return bool value
+          messageBubble.add(msgBubble);
+          // print(messageWidget);
+        }
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            children: messageBubble,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble(this.text, this.sender, this.isMe);
+  final String sender;
+  final String text;
+  final bool isMe;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            sender,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.black54,
+            ),
+          ),
+          Material(
+            borderRadius: isMe ? BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                bottomLeft: Radius.circular(30.0),
+                bottomRight: Radius.circular(30.0)) : BorderRadius.only(bottomLeft: Radius.circular(30.0), bottomRight: Radius.circular(30.0),topRight: Radius.circular(30.0)),
+            elevation: 5.0,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                '$text',
+                style: TextStyle(
+                  color: isMe ? Colors.white : Colors.black54,
+                   fontSize: 15.0),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
